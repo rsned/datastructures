6919d6b161db6880a91170bf9910ea5055b69402
Robert Snedegar <viking@gmail.com>
2024-08-08 20:09:25 -0700

    Define the initial go.mod and go.sum for this package tree.

---
+++ doc.go
@@ -0,0 +1,5 @@
+/*
+Package datastructures is the top level placeholder for my cornucopea of
+random datatructures I have used or needed along the way.
+*/
+package datastructures
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/rsned/datastructures
+
+go 1.22.4
+
+require (
+	github.com/google/go-cmp v0.6.0
+	golang.org/x/exp v0.0.0-20240613232115-7f521ea00fb8
+)
